0c7496f91278e232e244dfa2849b53a61c9b2104
Bradley_Williams <Bradley_Williams@a68ebfce-d462-47f5-9121-5ec068eaf1ac>
2016-02-29 21:34:20 +0000

    Added Client code, implements the User interface, Admin interface to come.

---
+++ ConclaveClient/src/conclaveclient/LoginController.java
@@ -0,0 +1,114 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package conclaveclient;
+
+import conclave.interfaces.UserInterface;
+import conclave.model.ConnectionsLog;
+import static conclaveclient.rmi.RMISecurityPolicyLoader.LoadDefaultPolicy;
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+import java.util.IllegalFormatException;
+
+/**
+ *
+ * @author BradleyW
+ */
+public class LoginController {
+    
+    private boolean connected;
+    private PacketUtil packetUtil;
+    
+    public LoginController()
+    {
+        connected = false;
+        LoadDefaultPolicy();
+    }
+    
+    public String connect(String iip, String iport)
+    {
+        String returnString = "ERROR CONNECTING 2";
+        try {
+        InetAddress ip = InetAddress.getByName(iip);
+        int port = Integer.parseInt(iport);
+        packetUtil = new PacketUtil(ip, port);
+        packetUtil.sendPacketRequest("PING");
+        returnString = packetUtil.readStream();
+        if (returnString.contains("100"))
+        {
+            connected = true;
+        }
+        return returnString;
+        } catch (UnknownHostException e)
+        {
+            returnString = "Cannot find a conclave server on that IP or Port";
+        } catch (IOException e) {
+            returnString = "Cannot find a conclave server on that IP or Port";
+        } catch (IllegalFormatException e)
+        {
+            returnString = "Bad format of IP/Port";
+        }
+        return returnString;
+    }
+    
+    public String login(String username, String password) throws RemoteException {
+        String returnString = "ERROR LOGGING IN";
+        if (connected)
+        {
+        try {
+        packetUtil.sendPacketRequest("SETUP-CONNECTION " + username + " " + password);
+        returnString = packetUtil.readStream();
+        String[] commandWords = returnString.split("//s+");
+        Registry reg = LocateRegistry.getRegistry(9807);
+        String[] regNames = reg.list();
+        for (String sstr : regNames)
+        {
+            System.out.println("Registry Entry: " + sstr);
+        }
+        UserInterface ui = (UserInterface) reg.lookup(username);
+        if (commandWords[0].contains("100") && (ui != null))
+        {
+            SwingGUI newSwingGui = new SwingGUI(ui);
+            System.out.println("Added swingGui to Observer.");
+        }
+        } catch (UnknownHostException e) {
+            returnString = "That conclave server is currently down";
+        } catch (NotBoundException e)
+        {
+            returnString = "Cannot connect to server registry";
+        }
+        } else {
+            returnString = "You must first connect to a conclave server";
+        }
+        System.out.println(returnString);
+        return returnString;
+    }
+    
+    public boolean isConnected() {
+        return connected;
+    }
+    
+
+    public String createAccount(String username, String password) {
+        String returnString = "";
+        if (connected)
+        {
+        try {
+        packetUtil.sendPacketRequest("SETUP-ACCOUNT " + username + " " + password);
+        returnString = packetUtil.readStream();
+        } catch (UnknownHostException e) {
+            returnString = "That conclave server is currently down";
+        }
+        } else {
+            returnString = "You must first connect to a conclave server";
+        }
+        return returnString;
+    }
+}
---
+++ ConclaveClient/src/conclaveclient/PacketUtil.java
@@ -0,0 +1,139 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package conclaveclient;
+
+import conclave.db.Account;
+import java.io.BufferedOutputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.ObjectInputStream;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+/**
+ *
+ * @author BradleyW
+ */
+public class PacketUtil {
+    
+    private Socket outSocket;
+    private OutputStream os;
+    private InputStream is;
+    
+    private InetAddress ip;
+    private int port;
+    private final int timeOutPeriod = 3000;
+    
+    public PacketUtil(InetAddress ip, int port) throws IOException
+    {
+        this.ip = ip;
+        this.port = port;
+        outSocket = new Socket(ip, port); 
+        os = outSocket.getOutputStream();
+        is  = outSocket.getInputStream();
+    }
+    
+    public void refreshSocket()
+    {
+        try {
+        outSocket = new Socket(ip, port); 
+        os = outSocket.getOutputStream();
+        is  = outSocket.getInputStream();
+        }catch (IOException e)
+        {
+        }
+    }
+    
+    public void setConnectionDetails(InetAddress ip, int port)
+    {
+        this.ip = ip;
+        this.port = port;
+        //refreshSocket();
+    }
+    
+    public void sendPacketRequest(String request) throws UnknownHostException
+    {
+        //refreshSocket();
+        try {
+            BufferedOutputStream bos = new BufferedOutputStream(os);
+            OutputStreamWriter osw = new OutputStreamWriter(bos);
+            String msg = request + "\n";
+            System.out.println("Request: " + msg);
+            osw.write(msg);
+            osw.flush();
+        } catch (IOException e)
+        {
+            e.printStackTrace();
+        } 
+    }
+    
+    public Object getObj()
+    {
+        Object acct = null;
+        try {
+            ObjectInputStream ois = new ObjectInputStream(is);
+            int timeOut = 0;
+            while (outSocket.isConnected())
+            {
+                acct = (Account) ois.readObject();
+                if (acct!=null) {
+                    return acct;
+                } else if (timeOut > 6000)
+                {
+                    break;
+                }
+                Thread.sleep(1);
+                timeOut++;
+            }
+        } catch (IOException e) {
+            
+        } catch (InterruptedException e)
+        {
+        } catch (ClassNotFoundException ce)
+        {
+            
+        }
+        return acct;
+    }
+    
+    public String readStream()
+    {
+        InputStreamReader insr = new InputStreamReader(is);
+        BufferedReader br = new BufferedReader(insr);
+        String entireRequest = "";
+        String nextLine = "";
+        int timeOut = 0;
+        try {
+            while (timeOut < timeOutPeriod) {
+                if ((nextLine = br.readLine()) != null) {
+                    entireRequest = entireRequest + nextLine;
+                    if (!br.ready()) {
+                        System.out.println("Completed in: " + timeOut + "ms");
+                        break;
+                    } else {
+                        entireRequest = entireRequest + "\n";
+                    }
+                } else {
+                timeOut++;
+                Thread.sleep(1);
+                }
+            }
+        System.out.println("Recieved: " + entireRequest);
+        } catch (InterruptedException e)
+        {
+            e.printStackTrace();
+        } catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+        return entireRequest;
+    }
+}
